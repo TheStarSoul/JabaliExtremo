f7b531dd88a6587aa36e501a11e4b21576bcc5be
TheStarSoul <thesoulr123@gmail.com>
2024-11-14 17:21:28 -0300

    14/11/2024 17:21

--- app/Views/layouts/tabla_layouts/resultadosTabla.php
+++ app/Views/layouts/tabla_layouts/resultadosTabla.php
@@ -1,49 +1,53 @@
-      <div class="container my-5">
+        <div class="container my-5">
         <h2 class="text-center mb-4">Tabla</h2>
         <!-- Tabla básica con clases de Bootstrap -->
-        <table class="table table-striped table-bordered">
-            <thead class="table-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Nombres</th>
-                    <th>Apellidos</th>
-                    <th>DNI</th>
-                    <th>Telefono</th>
-                    <th>Número de competición</th>
-                    <th>Correo</th>
-                    <th>Categoria</th>
-                    <th>Ciudad</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td><?= esc($data['piloto']['name_driver'])?></td>
-                    <td><?= esc($data['piloto']['lastname_driver'])?></td>
-                    <td><?= esc($data['piloto']['dni_driver'])?></td>
-                    <td><?= esc($data['piloto']['fono_driver'])?></td>
-                    <td><?= esc($data['piloto']['number_driver'])?></td>
-                    <td><?= esc($data['piloto']['email_driver'])?></td>
-                    <td><?= esc($data['piloto']['categoria_driver'])?></td>
-                    <td><?= esc($data['piloto']['country_driver'])?></td>
-                </tr>
-            </tbody>
-        </table>
-        <br>
-        <table class="table table-striped table-bordered">
-            <thead class="table-dark">
-                <tr>
-                    <th>Nombre del contacto de emergencia</th>
-                    <th>Telefono del contacto de emergencia</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?= esc($data['piloto']['nameContact_emergency'])?></td>
-                    <td><?= esc($data['piloto']['fonoContact_emergency'])?></td>
-                </tr>
-            </tbody>
-        </table>
-      </div>
-  </body>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>#</th>
+                            <th>Nombres</th>
+                            <th>Apellidos</th>
+                            <th>DNI</th>
+                            <th>Telefono</th>
+                            <th>Número de competición</th>
+                            <th>Correo</th>
+                            <th>Categoria</th>
+                            <th>Ciudad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td><?= esc($data['piloto']['name_driver'])?></td>
+                            <td><?= esc($data['piloto']['lastname_driver'])?></td>
+                            <td><?= esc($data['piloto']['dni_driver'])?></td>
+                            <td><?= esc($data['piloto']['fono_driver'])?></td>
+                            <td><?= esc($data['piloto']['number_driver'])?></td>
+                            <td><?= esc($data['piloto']['email_driver'])?></td>
+                            <td><?= esc($data['piloto']['categoria_driver'])?></td>
+                            <td><?= esc($data['piloto']['country_driver'])?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <br>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Nombre del contacto de emergencia</th>
+                            <th>Telefono del contacto de emergencia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?= esc($data['piloto']['nameContact_emergency'])?></td>
+                            <td><?= esc($data['piloto']['fonoContact_emergency'])?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
 </html>
